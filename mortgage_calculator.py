1d4f564134535a271c685de54199d09395b28e61
Haeun Maeng <haeun.maneg161826@gmail.com>
2025-02-26 14:07:26 +0900

    removed redundant loops and added invalid_number function

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -1,34 +1,45 @@
 def prompt(message):
     print(f'==>{message}')
 
+def invalid_input(input):
+    try:
+        number = float(input)
+        if number <= 0:
+            raise ValueError('Value must be > 0')
+    except ValueError:
+        return True
+    return False
+        
 prompt('Welcome to Mortgage/Car Payment Calculator!')
 
 while True:
-    while True:
-        prompt('Please enter the loan amount in dollars.')
-        loan_amount = input()
-        if float(loan_amount):
-            break
+    prompt('Please enter the loan amount in dollars.')
+    loan_amount = input()
+    while invalid_input(loan_amount):
         prompt('Hmm..invalid input. Please enter a valid loan amount')
-    while True:
-        prompt('Please enter the Annual Percentage Rate(APR).')
-        apr = input()
-        if float(apr):
-            break
+        loan_amount = input()
+
+    prompt('Please enter the Annual Percentage Rate(APR).')
+    apr = input()
+    while invalid_input(apr):
         prompt('Hmm..invalid input. Please enter a valid APR (just number)')
-    while True:
-        prompt('Please enter the loan duration in years')
-        loan_duration = input()
-        if float(loan_duration):
-            break
+        apr = input()
+
+    prompt('Please enter the loan duration in years')
+    loan_duration = input()
+    while invalid_input(loan_duration):
         prompt('Hmm..invalid input. Please enter a valid loan duration years')
+        loan_duration = input()
+
     monthly_interest_rate = float(apr) / 100 / 12
     loan_duration_months = int(loan_duration) * 12
     monthly_payment = float(loan_amount) * monthly_interest_rate / (
         1 - (1 + monthly_interest_rate) ** (
             -loan_duration_months))
+    
     prompt(f'The monthly payment amount is $'
            f'{monthly_payment:.2f}.')
+    
     prompt('Would you like to do another calculation? (y/n)')
     response = input().lower()
     while True:
