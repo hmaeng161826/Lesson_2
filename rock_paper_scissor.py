cef6202ea02fc795b7387009a922f1618e82c7d7
Haeun Maeng <haeun.maneg161826@gmail.com>
2025-02-28 00:15:47 +0900

    final version

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,52 +1,37 @@
 import random
+import pdb
+
+VALID_CHOICES = ['rock', 'paper', 'scissors']
 
 def prompt(message):
     print(f'==>{message}')
 
-def invalid(string):
-    global choices
-    if string in choices:
-        return False
-    return True
+def invalid(string, choices):
+    return string not in choices
 
 prompt('Welcome to Rock Paper Scissors')
 
 while True:
-    choices = ['rock', 'paper', 'scissors']
-    prompt("Enter 'rock', 'paper', or 'scissors: ")
+    prompt(f"Choose one: {', '.join(VALID_CHOICES)} ")
     user_choice = input()
 
-    while invalid(user_choice):
+    while invalid(user_choice, VALID_CHOICES):
         prompt("Please enter a valid input: 'rock', 'paper', or 'scissors")
         user_choice = input()
 
-    computer_choice = random.choice(choices)
+    computer_choice = random.choice(VALID_CHOICES)
     prompt(f'The computer chose {computer_choice}!')
 
-    if user_choice == 'rock':
-        match computer_choice:
-            case 'rock':
-                prompt("It's a tie!")
-            case 'paper':
-                prompt('Computer wins!')            
-            case 'scissors':
-                prompt('You win!')
-    elif user_choice == 'paper':
-        match computer_choice:
-            case 'rock':
-                prompt("You win!")
-            case 'paper':
-                prompt("It's a tie!")            
-            case 'scissors':
-                prompt('Computer wins!')
-    elif user_choice == 'scissors':
-        match computer_choice:
-            case 'rock':
-                prompt("Computer wins!")
-            case 'paper':
-                prompt("You win!")            
-            case 'scissors':
-                prompt("It's a tie!")
+    if ((user_choice == 'rock' and computer_choice == 'paper') or
+    (user_choice == 'paper' and computer_choice == 'scissors') or
+    (user_choice == 'scissors' and computer_choice == 'rock')):
+        prompt("Computer wins!")
+    elif ((user_choice == 'rock' and computer_choice == 'scissors') or
+    (user_choice == 'scissors' and computer_choice == 'paper') or
+    (user_choice == 'paper' and computer_choice == 'rock')):
+        prompt("You win!")
+    else:
+        prompt("It's a tie!")
 
     prompt('Do you want to play again (y/n)?') 
     response = input().lower()
