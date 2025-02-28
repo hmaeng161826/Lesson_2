24a319062b25641b9a9389279077d953735b81e6
Haeun Maeng <haeun.maneg161826@gmail.com>
2025-02-28 11:26:50 +0900

    lizard and spock added

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,6 +1,6 @@
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 def prompt(message):
     print(f'==>{message}')
@@ -8,26 +8,34 @@ def prompt(message):
 def invalid(string, choices):
     return string not in choices
 
-prompt('Welcome to Rock Paper Scissors')
+prompt('Welcome to Rock Paper Scissors Spock Lizard')
 
 while True:
     prompt(f"Choose one: {', '.join(VALID_CHOICES)} ")
     user_choice = input()
 
     while invalid(user_choice, VALID_CHOICES):
-        prompt("Please enter a valid input: 'rock', 'paper', or 'scissors")
+        prompt(f"Please enter a valid input: {VALID_CHOICES}")
         user_choice = input()
 
     computer_choice = random.choice(VALID_CHOICES)
     prompt(f'The computer chose {computer_choice}!')
 
-    if ((user_choice == 'rock' and computer_choice == 'paper') or
-    (user_choice == 'paper' and computer_choice == 'scissors') or
-    (user_choice == 'scissors' and computer_choice == 'rock')):
+    if (
+    ((user_choice == 'rock' or user_choice == 'spock') and computer_choice == 'paper') or
+    ((user_choice == 'paper' or user_choice =='lizard') and computer_choice == 'scissors') or
+    ((user_choice == 'scissors' or user_choice == 'lizard') and computer_choice == 'rock') or
+    ((user_choice == 'paper' or user_choice == 'spock') and computer_choice == 'lizard') or
+    ((user_choice == 'scissors' or user_choice == 'rock') and computer_choice == 'spock')
+    ):
         prompt("Computer wins!")
-    elif ((user_choice == 'rock' and computer_choice == 'scissors') or
-    (user_choice == 'scissors' and computer_choice == 'paper') or
-    (user_choice == 'paper' and computer_choice == 'rock')):
+    elif (
+    (user_choice == 'rock' and (computer_choice == 'scissors' or computer_choice == 'lizard')) or
+    (user_choice == 'scissors' and (computer_choice == 'paper' or computer_choice == 'lizard')) or
+    (user_choice == 'paper' and (computer_choice == 'rock' or computer_choice == 'spock')) or
+    (user_choice == 'lizard' and (computer_choice == 'paper' or computer_choice == 'spock')) or
+    (user_choice == 'spock' and (computer_choice == 'scissors' or computer_choice == 'rock'))
+    ):
         prompt("You win!")
     else:
         prompt("It's a tie!")
